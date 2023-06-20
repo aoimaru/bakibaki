f9c473b0a76b25757ae4aadb9070e5614df77146
aoimaru <syabi2622aoi@gmail.com>
2023-06-20 20:12:32 +0900

    [src] addを実装

--- cmd/add.go
+++ cmd/add.go
@@ -30,179 +30,32 @@ to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		current, _ := os.Getwd()
 		BakiBakiRootPath, err := lib.FindBakiBakiRoot(current)
-		GitRootPath, err := lib.FindGitRoot(current)
 		if err != nil {
 			fmt.Println(err)
 		}
-
-		bakibaki_client := lib.Client{
-			Root: BakiBakiRootPath,
-		}
-		name := args[0]
-		buffers, hash, err := bakibaki_client.CreateBlobFile(name)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(string(buffers), hash)
-
-		// indexファイルをオブジェクトとして取得
 
 		client := lib.Client{
-			Root: GitRootPath,
+			Root: BakiBakiRootPath,
+		}
+		path := args[0]
+		buffer, hash, err := client.CreateBlobFile(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(buffer), hash)
+
+		// indexファイルをオブジェクトとして取得
+		index_path := client.GetIndexPath()
+		index, err := client.GetIndexObject(index_path)
+		if err != nil {
+			fmt.Println(err)
 		}
 
-		indexPath := client.GetIndexPath()
-		fmt.Println(indexPath)
-		// index, err := lib.GetIndexObject(indexPath)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-
-		// // name := "typical90/032_TLE-2.py"
-		// // hash := "f131105cf1b5940a07b76f2608ea605f1ebcf2c7"
-
-		// filePath := current + "/" + name
-		// // fmt.Println(hash)
-
-		// var sysC syscall.Stat_t
-		// syscall.Stat(filePath, &sysC)
-
-		// fileInfo, err := os.Stat(filePath)
-		// if err != nil {
-		// 	fmt.Println(filePath, err)
-		// 	return
-		// }
-
-		// // fmt.Println(fileInfo)
-
-		// oct := fmt.Sprintf("%o", uint32(sysC.Mode))
-		// num, err := strconv.ParseUint(oct, 10, 32)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-		// mode := uint32(num)
-
-		// new_entry := lib.Entry{
-		// 	CTime: fileInfo.ModTime(),
-		// 	MTime: fileInfo.ModTime(),
-		// 	Dev:   uint32(sysC.Dev),
-		// 	Inode: uint32(sysC.Ino),
-		// 	Mode:  mode,
-		// 	Uid:   sysC.Uid,
-		// 	Gid:   sysC.Gid,
-		// 	Size:  uint32(sysC.Size),
-		// 	Hash:  hash,
-		// 	Name:  name,
-		// }
-
-		// var new_index lib.Index
-
-		// for _, entry := range index.Entries {
-		// 	if entry.Name == name {
-		// 		continue
-		// 	}
-		// 	if entry.Hash == hash {
-		// 		continue
-		// 	}
-		// 	fmt.Printf("%+v\n", entry)
-		// 	new_index.Entries = append(new_index.Entries, entry)
-		// }
-		// new_index.Entries = append(new_index.Entries, new_entry)
-
-		// // fmt.Println(len(index.Entries))
-		// for _, entry := range new_index.Entries {
-		// 	fmt.Println("Index", entry)
-		// }
-		// // fmt.Println(len(new_index.Entries))
-
-		// new_index.Dirc = "DIRC"
-		// new_index.Version = 2
-		// new_index.Number = uint32(len(new_index.Entries))
-
-		// buffer := make([]byte, 0)
-
-		// dirc := []byte(new_index.Dirc)
-		// version := util.Element2byte32(new_index.Version)
-		// number := util.Element2byte32(new_index.Number)
-
-		// buffer = append(buffer, dirc...)
-		// buffer = append(buffer, version...)
-		// buffer = append(buffer, number...)
-
-		// for _, entry := range new_index.Entries {
-
-		// 	c_unix := entry.CTime.Unix()
-		// 	buf_c_unix := util.Element2byte32(uint32(c_unix))
-		// 	buffer = append(buffer, buf_c_unix...)
-		// 	buffer = append(buffer, buf_c_unix...)
-
-		// 	m_unix := entry.MTime.Unix()
-		// 	buf_m_unix := util.Element2byte32(uint32(m_unix))
-		// 	buffer = append(buffer, buf_m_unix...)
-		// 	buffer = append(buffer, buf_m_unix...)
-
-		// 	dev := entry.Dev
-		// 	buf_dev := util.Element2byte32(uint32(dev))
-		// 	buffer = append(buffer, buf_dev...)
-
-		// 	inode := entry.Inode
-		// 	buffer_inode := util.Element2byte32(uint32(inode))
-		// 	buffer = append(buffer, buffer_inode...)
-
-		// 	mode := entry.Mode
-		// 	buffer_mode := util.Element2byte32(uint32(mode))
-		// 	buffer = append(buffer, buffer_mode...)
-
-		// 	uid := entry.Uid
-		// 	buffer_uid := util.Element2byte32(uint32(uid))
-		// 	buffer = append(buffer, buffer_uid...)
-
-		// 	gid := entry.Gid
-		// 	buffer_gid := util.Element2byte32(uint32(gid))
-		// 	buffer = append(buffer, buffer_gid...)
-
-		// 	size := entry.Size
-		// 	buffer_size := util.Element2byte32(uint32(size))
-		// 	buffer = append(buffer, buffer_size...)
-
-		// 	bHash, err := hex.DecodeString(entry.Hash)
-		// 	if err != nil {
-		// 		fmt.Println("ココ？")
-		// 		continue
-		// 	}
-		// 	buffer = append(buffer, bHash...)
-
-		// 	bnSize := make([]byte, 2)
-		// 	binary.BigEndian.PutUint16(bnSize, uint16(len(entry.Name)))
-		// 	buffer = append(buffer, bnSize...)
-
-		// 	bName := []byte(entry.Name)
-		// 	buffer = append(buffer, bName...)
-
-		// 	var sw uint64
-		// 	sw = 62
-
-		// 	padding := util.GetPaddingSize(sw + uint64(len(bName)))
-		// 	bPadding := make([]byte, padding)
-		// 	buffer = append(buffer, bPadding...)
-		// 	fmt.Println(entry)
-		// 	fmt.Println("OK")
-		// }
-
-		// w, err := os.Create(indexPath)
-		// // w, err := os.Create("/mnt/c/Users/81701/Documents/AtCoder/subsubsubIndex")
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-		// defer w.Close()
-
-		// count, err := w.Write(buffer)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-		// fmt.Printf("write %d bytes\n", count)
-		// fmt.Println("TEST")
+		new_index := index.UpdateIndex(path, hash)
+		git_buffer := new_index.AsByte()
+		if err := git_buffer.ToFile(index_path); err != nil {
+			fmt.Println(err)
+		}
 
 	},
 	Args: func(cmd *cobra.Command, args []string) error {
